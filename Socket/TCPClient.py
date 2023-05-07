23a5ebb3259d3784330b0cbc766a863a38fc5320
naccaratocarolina <naccaratocarolina@dcc.ufrj.br>
2023-05-07 11:55:14 -0300

    TCP + UDP

--- Socket/TCPClient.py
+++ Socket/TCPClient.py
@@ -1,6 +1,6 @@
 from socket import *
 
-# Vincula o socket cliente para o endereço de loopback
+# Define o nome ou endereco IP do servidor
 serverName = '127.0.0.1'
 
 # Define a porta em que o servidor esta escutando
@@ -12,8 +12,17 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 # Estabelece a conexão
 clientSocket.connect((serverName, serverPort))
 
+# Recebe a mensagem do usuario
 sentence = input('Input a word to be translated: ')
+
+# Envia a mensagem para o servidor
 clientSocket.send(sentence.encode())
+
+# Recebe a mensagem modificada enviada pelo servidor
 modifiedSentence = clientSocket.recv(1024)
-print('From Server:', modifiedSentence.decode())
+
+# Imprime a mensagem modificada recebida do servidor
+print('From Server: ', modifiedSentence.decode())
+
+# Fecha o socket do cliente
 clientSocket.close()
--- Socket/TCPServer.py
+++ Socket/TCPServer.py
@@ -3,7 +3,7 @@ from socket import *
 # Inicializa servidor na porta 12000
 serverPort = 12000
 
-# Cria um novo TCP socket
+# Cria o socket do servidor com protocolo TCP
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # Vincula o socket do servidor ao endereço de loopback
@@ -23,23 +23,28 @@ networkDict = {
   'IP address': 'endereço IP',
   'subnet mask': 'máscara de sub-rede',
   'MAC address': 'endereço MAC',
-  'packet': 'pacote',
   'port forwarding': 'encaminhamento de porta'
 }
 
+# Funcao para traduzir uma palavra baseada no dicionario
 def translate(dic, word):
   for key, value in dic.items():
-    if (key == word.lower()):
+    if (key.lower() == word.lower()):
       return value
   return 'Word not found'
 
 while True:
+  # Aceita a conexao do cliente
   connectionSocket, addr = serverSocket.accept()
 
   # Palavra recebida do cliente
   sentence = connectionSocket.recv(1024).decode()
 
-  # Traducao da palavra baseado no dicionario
+  # Traduz a palavra baseada no dicionario
   translated = translate(networkDict, sentence)
+
+  # Envia a palavra traduzida de volta para o cliente
   connectionSocket.send(translated.encode())
+
+  # Fecha a conexao com o cliente
   connectionSocket.close()
